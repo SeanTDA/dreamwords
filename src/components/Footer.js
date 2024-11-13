fb84bcf33d8c8aee79cc22b3fa8855611a7b8efa
SeanTDA <93623678+SeanTDA@users.noreply.github.com>
2024-11-13 16:14:04 +1100

    removed dalle from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,9 @@ function Footer () {
 
     return (
         <div className = "footer">
-            Copyright 2023 - <span className="footer-hyperlink"><a className="footer-hyperlink" href="https://www.t-da.io/" target="_blank" rel="noopener noreferrer">T&DA</a>&nbsp;</span> Powered by <span className="footer-hyperlink">DALLE 3</span>
+            Copyright 2023 - <span className="footer-hyperlink"><a className="footer-hyperlink" href="https://www.t-da.io/" target="_blank" rel="noopener noreferrer">T&DA</a></span>
         </div>
     )
 }
 
 export default Footer;
-
